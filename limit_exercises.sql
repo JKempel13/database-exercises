c0b9ca3cf547f338a5f75cfa9f47d93567e1800d
Jason Kempel <kempel.jason13@gmail.com>
2019-10-11 16:27:11 -0500

    updated exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -8,4 +8,10 @@ SELECT first_name, last_name from employees
 WHERE hire_date LIKE '199%'
   AND birth_date LIKE '%12-25'
 ORDER BY birth_date, hire_date desc
+LIMIT 5;
+
+SELECT first_name, last_name from employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date desc
 LIMIT 5 OFFSET 45;
