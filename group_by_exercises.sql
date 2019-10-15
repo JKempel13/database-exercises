89f237d6ce5ae155a07fe23b2705a6bd8775d1a3
Jason Kempel <kempel.jason13@gmail.com>
2019-10-15 13:57:19 -0500

    exercise walkthrough

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,21 +1,24 @@
-
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name from employees
+SELECT last_name, count(*) from employees
 WHERE last_name
-    LIKE 'E%'
-   AND last_name LIKE '%e'
+    LIKE 'E%E'
 GROUP BY last_name;
 
-SELECT DISTINCT last_name from employees
+SELECT last_name, first_name from employees
 WHERE last_name
-    LIKE 'E%'
-   AND last_name LIKE '%e'
-GROUP BY first_name, last_name;
+    LIKE 'E%E'
+GROUP BY last_name, first_name;
 
 SELECT last_name from employees
 WHERE last_name LIKE '%q%'
   AND last_name NOT LIKE '%qu%'
   GROUP BY last_name;
+
+SELECT CONCAT(first_name, ' ' , last_name) AS name, COUNT(*) AS '# of people with the same name' from employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY COUNT(*) desc;
 
 SELECT COUNT(*) AS 'How many with these names', gender AS 'M/F' from employees WHERE first_name IN ('Irena','Vidya','Maya') GROUP BY (gender);
